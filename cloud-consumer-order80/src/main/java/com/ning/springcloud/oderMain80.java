a5aeafa5738ed4e8d1f023a54070926f0809fdd1
lining95 <1075507782@qq.com>
2021-06-03 09:09:42 +0800

    Revert "Revert "spingcloud代码""
    This reverts commit 2c84195b612a0af9c12cbdf7427e4b8252a14be2.

---
+++ cloud-consumer-order80/src/main/java/com/ning/springcloud/oderMain80.java
@@ -0,0 +1,11 @@
+package com.ning.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class oderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(oderMain80.class, args);
+    }
+}
---
+++ cloud-provider-payment8001/src/main/java/com/ning/springcloud/service/PaymentService.java
@@ -0,0 +1,10 @@
+package com.ning.springcloud.service;
+
+import com.ning.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
